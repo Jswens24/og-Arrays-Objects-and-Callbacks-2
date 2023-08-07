97e2539c18a56f78ec9241a498690e52a05972f8
Jessica Swenson <jessicamswens@gmail.com>
2023-08-07 14:03:34 -0600

    started for-in

--- for-in-delete.js
+++ for-in-delete.js
@@ -11,25 +11,25 @@
   In the example below, we are accessing the property values. Uncomment the code below, run it and look at what prints in the console.
 */
 
-// var values = {
-//   one: 'These',
-//   two: ' are',
-//   three: ' the',
-//   four: ' property',
-//   five: ' values.'
-// }
+var values = {
+  one: 'These',
+  two: ' are',
+  three: ' the',
+  four: ' property',
+  five: ' values.'
+}
 
-// for(var key in values) {
-//   console.log(values[key])
-// }
+for (var key in values) {
+  console.log(values[key])
+}
 
 /*
   In this next example, we are accessing the property names themselves. Uncomment the code below, run it and look at what prints in the console.
 */
 
-// for(var key in values) {
-//   console.log(key)
-// }
+for (var key in values) {
+  console.log(key)
+}
 
 ////////// PROBLEM 1 //////////
 
@@ -37,8 +37,12 @@
   Inside the function showValues, write a for in loop that concatenates each of the property values and returns the concatenated string.
 */
 
-function showValues(obj) {
-  //Code Here
+function showValues(values) {
+  let str = ''
+  for (let key in values) {
+    str = str + values[key]
+  }
+  return str
 }
 
 ////////// PROBLEM 2 //////////
@@ -49,7 +53,14 @@ function showValues(obj) {
   Return the updated object.
 */
 
-//Code Here
+const greaterThan10 = (obj) => {
+  for (let key in obj) {
+    if (obj[key] > 10) {
+      obj[key] = 0
+    }
+  }
+  return obj
+}
 
 ////////// PROBLEM 3 //////////
 
@@ -59,7 +70,12 @@ function showValues(obj) {
   Return the updated object.
 */
 
-//Code Here
+const double = (obj) => {
+  for (let key in obj) {
+    obj[key] = obj[key] * 2
+  }
+  return obj
+}
 
 ////////// PROBLEM 4 //////////
 
